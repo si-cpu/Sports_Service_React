0fbe81701c890280e9432b32b2a8721dfd6f5817
leehah <leehah0908@naver.com>
2024-10-22 09:52:26 +0900

    basic logic upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AccountModify from "./pages/AccountModify";
+import Board from "./pages/Board";
+import BoardDetail from "./pages/BoardDetail";
+import GameDetail from "./pages/GameDetail";
+import GameList from "./pages/GameList";
+import MainPage from "./pages/MainPage";
+import SignIn from "./pages/SignIn";
+import TeamDetail from "./pages/TeamDetail";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                {/* <Sidebar /> */}
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage />} />
+                        <Route path="/game/list" element={<GameList />} />
+                        <Route path="/game/detail/:game_id" element={<GameDetail />} />
+                        <Route path="/board/list" element={<Board />} />
+                        <Route path="/board/detail/:board_id" element={<BoardDetail />} />
+                        <Route path="/board/team/:team_id" element={<TeamDetail />} />
+                        <Route path="/signin" element={<SignIn />} />
+                        <Route path="/account/modify" element={<AccountModify />} />
+                    </Routes>
+                </main>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/AccountModify.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AccountModify() {
+    return <div>AccountModify</div>;
+}
+
+export default AccountModify;
